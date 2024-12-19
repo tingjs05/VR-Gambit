b7a2f114af24573afdf106fd2bdd5743c8119073
sirpotatosalad <105273715+sirpotatosalad@users.noreply.github.com>
2024-12-19 14:13:39 +0800

    Setup for card throwing

---
+++ Assets/Scripts/Physics/CardThrowing.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardThrowing : MonoBehaviour
+{
+
+    public Transform leftHandRayOrigin;
+    public Transform rightHandRayOrigin;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void ThrowCard()
+    {
+
+    }
+
+
+}
