6cd5930f2839492693b8107e3010b7cf1ec07c55
Samuel Ting <tingjs05@gmail.com>
2024-12-19 14:47:20 +0800

    background colour check

--- Assets/Samples/XR Hands/1.4.3/Gestures/Examples/Scripts/StaticHandGesture.cs
+++ Assets/Samples/XR Hands/1.4.3/Gestures/Examples/Scripts/StaticHandGesture.cs
@@ -153,7 +153,8 @@ namespace UnityEngine.XR.Hands.Samples.GestureSample
 
         void Awake()
         {
-            m_BackgroundDefaultColor = m_Background.color;
+            if (m_Background != null) 
+                m_BackgroundDefaultColor = m_Background.color;
 
             if (m_Highlight)
             {
