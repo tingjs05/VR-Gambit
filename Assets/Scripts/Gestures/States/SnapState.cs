fbc1ba1cbe7f4023a1162de64ea65cd0627b9903
Samuel Ting <tingjs05@gmail.com>
2024-12-27 14:13:34 +0800

    card offset works

--- Assets/Scripts/Gestures/States/SnapState.cs
+++ Assets/Scripts/Gestures/States/SnapState.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 using Patterns.FSM;
 
 namespace Gestures
@@ -12,8 +13,13 @@ namespace Gestures
         public override void Enter()
         {
             base.Enter();
+            // calculate card offset
+            Vector3 cardOffset = 
+                (Camera.main.transform.forward * character.gestureSettings.card_placement_offset.x) + 
+                (Camera.main.transform.right * character.gestureSettings.card_placement_offset.z) + 
+                (Camera.main.transform.up * character.gestureSettings.card_placement_offset.y);
             // hover card
-            character.cardPlacementManager.PlaceCard(character.hand_position, character.hand_rotation);
+            character.cardPlacementManager.PlaceCard(character.hand_position, cardOffset, character.hand_rotation);
         }
     }
 }
