df426d819eb40f272896266ccfeaf673a2268c33
Samuel Ting <tingjs05@gmail.com>
2025-01-17 14:06:25 +0800

    snap card auto aim

--- Assets/Scripts/Card Action/CardPlacement.cs
+++ Assets/Scripts/Card Action/CardPlacement.cs
@@ -9,10 +9,14 @@ namespace Card
         public float positionReachedThreshold = 0.01f;
 
         // Method to place a card
-        public void PlaceCard(Vector3 handPosition, Vector3 offset, Quaternion handRotation)
+        public void PlaceCard(Vector3 handPosition, Vector3 offset, Quaternion handRotation, Transform selectedTarget)
         {
             CardObject card = ActionManager.Instance.InstantiateCard(handPosition, handRotation);
             card.HoverCard();
+            // if there is a selected target, point towards it
+            if (selectedTarget != null) 
+                card.transform.up = -(handPosition + offset - selectedTarget.position).normalized;
+            // start coroutine to float to float position
             StartCoroutine(FloatToPosition(card, handPosition + offset));
         }
 
